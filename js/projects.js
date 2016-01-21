39884745bb4f028efc60e0b94cf9fc2e52677f24
Ryan Perry <ricoolohsix@hotmail.com>
2016-01-21 12:19:42 +0000

    Improved scalability of the images on the projects tab

--- js/projects.js
+++ js/projects.js
@@ -15,7 +15,7 @@ function handleProjectsXHR(){
             div.className = "projectBox fadeIn small";
 
             var content = "";
-            content = content + "<img class=\"projectImg\" src=\"https://raw.githubusercontent.com/" + project.full_name + "/master/demo.jpg\">";
+            content = content + "<img class=\"projectImg\" src=\"https://raw.githubusercontent.com/" + project.full_name + "/master/demo.jpg\" width=\"100%\">";
             content = content + "<h2>" + project.name + "</h2>";
             content = content + "<p>" + project.description + "</p>";
             div.addEventListener("click", function(){ window.open(project.html_url); }, false);
