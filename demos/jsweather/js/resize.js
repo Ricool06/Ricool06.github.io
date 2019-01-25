8ee652a120d6ef38119c3e174b4cadd81b9b621a
Ricool06 <ricoolohsix@hotmail.com>
2019-01-25 23:47:03 +0000

    Replace site with Gatsby starter

--- demos/jsweather/js/resize.js
+++
@@ -1,8 +0,0 @@
-var canvas = document.getElementById("c");
-window.addEventListener("resize",resizeCanvas);
-resizeCanvas();
-
-function resizeCanvas(){
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-}
